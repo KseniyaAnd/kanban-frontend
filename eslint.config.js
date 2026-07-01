e62facc5e80538572db8c09b0602cca16fd6c4b5
Kseniya <kseniya.andrusenka72@gmail.com>
2026-07-01 10:59:44 +0200

    feat: add prettier rules in eslint, remove proxy from vite cobfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,13 @@ import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config(
   { ignores: ['dist', 'src/routeTree.gen.ts', '*.config.ts', '*.config.js'] },
   js.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
-  prettierConfig,
+  
   {
     plugins: { 'react-hooks': reactHooks },
     rules: {
@@ -21,4 +22,22 @@ export default tseslint.config(
       },
     },
   },
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'all',
+        }
+      ],
+    },
+  },
+
+  prettierConfig,
 )
