961f2cd9cb141008b015917f5522802f29e7f588
Kseniya <kseniya.andrusenka72@gmail.com>
2026-07-02 12:11:41 +0200

    fix: api connection

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,19 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: (import.meta.env.VITE_API_URL as string) || '/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
 export const setAuthToken = (token: string) => {
+  // давай хранить токет в сторе Zustand
+  // и в интерсепторс можно получить токен из стора (в Zustand store доступен и вне компонентов) и использовать его
   apiClient.defaults.headers.common.Authorization = `Bearer ${token}`
 }
+
+// также в интесепторс нужно будет настроить логику получения нового токена
+// когда access token истекает, все упавшие запросы в это время с 401 ошибкой нужно поместить
+// в очередь и продолжить выполнение запросов после получения нового токена
 
 export const removeAuthToken = () => {
   delete apiClient.defaults.headers.common.Authorization
